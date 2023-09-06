0c9108099271f1a1a8541964b3fb9b4e05868f92
Hamza Chbit <118445750+HamzaChbit@users.noreply.github.com>
2023-09-06 14:40:50 +0100

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
-     credentials: 'include',
+   
     });
     if (response.ok) {
       response.json().then(userInfo => {
